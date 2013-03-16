734d542a43e4ba26d1bb021a2dc02063584cc886
Ivan Kryvoruchko <gildraug@gmail.com>
2013-03-16 19:22:40 +0200

    Separate namespace for configuration.

---
+++ src/urdar/config.clj
@@ -0,0 +1,43 @@
+(ns urdar.config
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [friend-oauth2.workflow :refer [format-config-uri]]))
+
+(defn read-config
+  "Reads edn config file."
+  []
+  (edn/read (java.io.PushbackReader. (io/reader (io/resource "config.edn")))))
+
+(def config (read-config))
+
+(def github-uri-config
+  {:authentication-uri
+   {:url (get-in config [:github-urls :authentication-url])
+    :query {:client_id (get-in config [:github-client-config :client-id])
+            :response_type "code"
+            :redirect_uri (format-config-uri (:github-client-config config))
+            :scope "user:email"}}
+
+   :access-token-uri
+   {:url (get-in config [:github-urls :access-token-url])
+    :query {:client_id (get-in config [:github-client-config :client-id])
+            :client_secret (get-in config [:github-client-config :client-secret])
+            :grant_type "authorization_code"
+            :redirect_uri (format-config-uri (:github-client-config config))
+            :code ""}}})
+
+(def google-uri-config
+  {:authentication-uri
+   {:url (get-in config [:google-urls :authentication-url])
+    :query {:client_id (get-in config [:google-client-config :client-id])
+            :response_type "code"
+            :redirect_uri (format-config-uri (:google-client-config config))
+            :scope "https://www.googleapis.com/auth/userinfo.email"}}
+
+   :access-token-uri
+   {:url (get-in config [:google-urls :access-token-url])
+    :query {:client_id (get-in config [:google-client-config :client-id])
+            :client_secret (get-in config [:google-client-config :client-secret])
+            :grant_type "authorization_code"
+            :redirect_uri (format-config-uri (:google-client-config config))
+            :code ""}}})
--- src/urdar/handler.clj
+++ src/urdar/handler.clj
@@ -1,58 +1,15 @@
 (ns urdar.handler
-  (:require [urdar.routes.site :as site]
+  (:require [urdar.config :as cfg]
+            [urdar.routes.site :as site]
             [compojure.core :as compojure :refer [defroutes ANY]]
             [compojure.route :as route]
             [compojure.handler :as handler]
             [cemerick.friend :as friend]
             [friend-oauth2.workflow :as oauth2]))
 
-(def config-auth {:roles #{::user}})
-
-(def github-client-config
-  {:client-id ""
-   :client-secret ""
-   :callback {:domain "http://localhost:3000" :path "/oauth2callback"}})
-
-(def github-uri-config
-  {:authentication-uri
-   {:url "https://github.com/login/oauth/authorize"
-    :query {:client_id (:client-id github-client-config)
-            :response_type "code"
-            :redirect_uri (oauth2/format-config-uri github-client-config)
-            :scope "user:email"}}
-
-   :access-token-uri
-   {:url "https://github.com/login/oauth/access_token"
-    :query {:client_id (:client-id github-client-config)
-            :client_secret (:client-secret github-client-config)
-            :grant_type "authorization_code"
-            :redirect_uri (oauth2/format-config-uri github-client-config)
-            :code ""}}})
-
-(def google-client-config
-  {:client-id ""
-   :client-secret ""
-   :callback {:domain "http://localhost:3000" :path "/oauth2callback"}})
-
-(def google-uri-config
-  {:authentication-uri
-   {:url "https://accounts.google.com/o/oauth2/auth"
-    :query {:client_id (:client-id google-client-config)
-            :response_type "code"
-            :redirect_uri (oauth2/format-config-uri google-client-config)
-            :scope "https://www.googleapis.com/auth/userinfo.email"}}
-
-   :access-token-uri
-   {:url "https://accounts.google.com/o/oauth2/token"
-    :query {:client_id (:client-id google-client-config)
-            :client_secret (:client-secret google-client-config)
-            :grant_type "authorization_code"
-            :redirect_uri (oauth2/format-config-uri google-client-config)
-            :code ""}}})
-
 (defroutes app
   (compojure/context "/" request
-                     (friend/wrap-authorize site/registered #{::user}))
+                     (friend/wrap-authorize site/registered #{:urdar/user}))
   (route/resources "/")
   (friend/logout (ANY "/logout" request (ring.util.response/redirect "/")))
   (route/not-found "<h1>Page not found</h1>"))
@@ -65,16 +22,16 @@
        {:workflows
         [(oauth2/workflow
           {:login-uri "/login-github"
-           :client-config github-client-config
-           :uri-config github-uri-config
+           :client-config (:github-client-config cfg/config)
+           :uri-config cfg/github-uri-config
            :access-token-parsefn #(-> % :body
                                       ring.util.codec/form-decode
                                       (get "access_token"))
-           :config-auth config-auth})
+           :config-auth (:config-auth cfg/config)})
 
          (oauth2/workflow
           {:login-uri "/login-google"
-           :client-config google-client-config
-           :uri-config google-uri-config
-           :config-auth config-auth})]})
+           :client-config (:google-client-config cfg/config)
+           :uri-config cfg/google-uri-config
+           :config-auth (:config-auth cfg/config)})]})
       handler/site))
