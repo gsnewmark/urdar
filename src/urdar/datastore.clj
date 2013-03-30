91f356412a3225518914c0642b2ea1a5755fbd3f
Ivan Kryvoruchko <gildraug@gmail.com>
2013-03-30 22:47:02 +0200

    Added basic support for tags and bookmarks.

--- src/urdar/datastore.clj
+++ src/urdar/datastore.clj
@@ -1,3 +1,4 @@
+;; TODO fix (to work with new nj api)
 (ns urdar.datastore
   "Operations with a datastore."
   (:require [urdar.datastore.neo4j :as nj]))
@@ -8,18 +9,38 @@
   (get-user [self e-mail] "Retrieves user with given e-mail from datastore.")
   (create-user [self e-mail] "Creates user with given e-mail in datastore."))
 
-;; ## UserDatastore Neo4j-backed implementation
+(defprotocol TagDatastore
+  (tag-exists? [self tag]
+    "Checks whether the given tag exists in datastore.")
+  (get-tag [self tag] "Retrieves given tag from datastore.")
+  (create-tag [self tag] "Creates user with given e-mail in datastore."))
 
-(defrecord Neo4jUserDatastore []
+;; ## Datastore Neo4j-backed implementation
+
+(defrecord Neo4jDatastore []
   UserDatastore
   (user? [_ e-mail]
     (not (nil? (nj/get-user e-mail nj/users-index))))
   (get-user [_ e-mail]
     (nj/get-user e-mail nj/users-index))
   (create-user [_ e-mail]
-    (nj/create-user e-mail nj/users-root nj/users-index)))
+    (nj/create-user e-mail nj/users-root nj/users-index))
+
+  TagDatastore
+  (tag-exists? [_ tag]
+    (not (nil? (nj/get-tag tag nj/tags-index))))
+  (get-tag [_ tag]
+    (nj/get-tag tag nj/tags-index))
+  (create-tag [_ tag]
+    (nj/create-tag tag nj/tags-root nj/tags-index)))
+
+(def neo4j-datastore (->Neo4jDatastore))
 
 ;; TODO make a part of configuration
 (def user-ds
   "Particular implementation of UserDatastore protocol."
-  (->Neo4jUserDatastore))
+  neo4j-datastore)
+
+(def tag-ds
+  "Particular implementation of TagDatastore protocol."
+  neo4j-datastore)
--- src/urdar/datastore/neo4j.clj
+++ src/urdar/datastore/neo4j.clj
@@ -1,3 +1,4 @@
+;; TODO remove code repetition
 (ns urdar.datastore.neo4j
   "Operations with a Neo4j-based datastore."
   (:require [urdar.config :as config]
@@ -9,35 +10,120 @@
 
 ;; ## Neo4j
 
-;; Initiate connection to Neo4j REST API.
-(let [{:keys [url login password]} (:neo4j config/config)]
+(defn init-connection
+  "Initiate connection to Neo4j REST API."
+  [url login password]
   (nr/connect! url login password))
 
-;; Create index for graphs roots.
-(defonce roots-index (nn/create-index "roots"))
+(comment
+  (let [{:keys [url login password]} (:neo4j config/config)]
+   (init-connection url login password)))
+
 ;; Create index for users.
 (defonce users-index (nn/create-index "users"))
+;; Create index for tags.
+(defonce tags-index (nn/create-index "tags"))
+;; Create index for bookmarks.
+(defonce bookmarks-index (nn/create-index "bookmarks"))
 
-;; Root of users graph.
-(defonce users-root (nn/find-one (:name roots-index) "root" "user"))
+;; TODO maybe use hashes for indices' keys
 
 (defn get-user
-  "Retrieves user with given e-mail from given index."
-  [e-mail index]
+  "Retrieves user node with given e-mail from given index."
+  [index e-mail]
   (nn/find-one (:name index) "e-mail" e-mail))
 
+(defn gen-utag
+  "Generates a key used for tags index."
+  [e-mail tag]
+  (str e-mail "+" tag))
+
+(defn get-tag
+  "Retrieves tag node with given name for given e-mail from given index."
+  [index e-mail tag-name]
+  (nn/find-one (:name index) "utag" (gen-utag e-mail tag-name)))
+
+(defn get-tags-for-user
+  "Retrieves all tags' names of a given user node."
+  [user-node]
+  ;; TODO maybe shouldn't be lazy
+  ;; TODO maybe return tag nodes, not names
+  (map
+   #(get % "tag.name")
+   (cy/tquery "START user=node({sid}) MATCH user-[:has]->tag RETURN tag.name"
+              {:sid (:id user-node)})))
+
+(defn get-bookmark
+  "Retrieves bookmark node with given link for given e-mail from given
+index."
+  [index link]
+  (nn/find-one (:name index) "bookmark" link))
+
+(defn get-bookmarks-for-user
+  "Retrieves all bookmarks' links which a given user added."
+  [user-node]
+  ;; TODO maybe shouldn't be lazy
+  ;; TODO maybe return tag nodes, not names
+  (map
+   #(get % "bookmark.link")
+   (cy/tquery (str "START user=node({sid}) MATCH user-[:bookmarked]->bookmark"
+                   " RETURN bookmark.link")
+              {:sid (:id user-node)})))
+
+(defn get-bookmarks-for-tag
+  "Retrieves all bookmarks' links which given tag contains."
+  [tag-node]
+  ;; TODO maybe shouldn't be lazy
+  ;; TODO maybe return tag nodes, not names
+  (map
+   #(get % "bookmark.link")
+   (cy/tquery (str "START tag=node({sid}) MATCH tag-[:contains]->bookmark"
+                   " RETURN bookmark.link")
+              {:sid (:id tag-node)})))
+
 (defn create-user
-  "Creates a user as a node in graph with given root and adds it in index."
-  [e-mail root index]
-  (let [user (get-user e-mail index)]
-    (if-not user
-      (if-let [user (nn/create {:e-mail e-mail})]
-        (do
-          (some-> user
-                  :id
-                  (nn/add-to-index (:name index) "e-mail" e-mail true)
-                  (as-> user
-                        (nrl/create root user :registered
-                                    {:date (pr-str (Date.))})))
-          user))
-      user)))
+  "Creates a user as a node in graph and adds it to given index.
+Returns nil if something goes wrong during creation."
+  [index e-mail]
+  (if-let [user (get-user index e-mail)]
+    user
+    (some-> (nn/create {:e-mail e-mail})
+            :id
+            (nn/add-to-index (:name index) "e-mail" e-mail true))))
+
+(defn create-tag
+  "Creates a tag as a node in graph for a given user node and adds it
+to given index. Returns nil if something goes wrong during creation."
+  [index user-node tag]
+  (if-let [e-mail (get-in user-node [:data :e-mail])]
+    (if-let [tag-node (get-tag index e-mail tag)]
+      tag-node
+      (some-> (nn/create {:name tag})
+              :id
+              (nn/add-to-index (:name index) "utag"
+                               (gen-utag e-mail tag) true)
+              (as-> t
+                    (let [tag-node t]
+                      (nrl/create user-node tag-node :has)
+                      tag-node))))))
+
+(defn create-bookmark
+  "Creates a bookmark as a node in graph for a given user node and adds it to
+given index. Returns nil if something goes wrong during creation."
+  [index user-node link]
+  (if-let [e-mail (get-in user-node [:data :e-mail])]
+    (if-let [bookmark-node (get-bookmark index link)]
+      bookmark-node
+      (some-> (nn/create {:link link})
+              :id
+              (nn/add-to-index (:name index) "bookmark" link true)
+              (as-> b
+                    (let [bookmark-node b]
+                      (nrl/create user-node bookmark-node :bookmarked
+                                  {:on (pr-str (Date.))})
+                      bookmark-node))))))
+
+(defn tag-bookmark
+  "Adds a given bookmark to a given tag."
+  [tag-node bookmark-node]
+  (nrl/create tag-node bookmark-node :contains))
