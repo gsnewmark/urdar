2d5ac29363a57c8dee1fd9427140cc3cf2b2add3
Ivan Kryvoruchko <gildraug@gmail.com>
2013-05-19 17:17:46 +0300

    Revert and comment test for recommendations.

--- src/urdar/db/neo4j.clj
+++ src/urdar/db/neo4j.clj
@@ -235,7 +235,6 @@
         (str "START "
              "link=node:" (:name l-index) "(\"" lkey ":" lvalue "\"), "
              "user=node:" (:name u-index) "({key}={value}) "
-             "MATCH ()-[:bookmarks]->(link) "
              "WITH link AS l, user AS u "
              "SKIP " to-skip  " LIMIT " random-links-limit
              " WHERE NOT((u)-[:has]->()-[:bookmarks]->(l)) "
